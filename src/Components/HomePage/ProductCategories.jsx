2ed7e05b84a43b885c2211bb45d9f58352406c75
Greta Ra <ggretute88@gmail.com>
2023-10-18 15:51:20 +0200

    fix homepage categories

--- src/Components/HomePage/ProductCategories.jsx
+++ src/Components/HomePage/ProductCategories.jsx
@@ -1,11 +1,10 @@
-// ProductCategories.js
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const ProductCategories = () => {
   const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Make an API request using the fetch API to get product categories
     fetch('https://fakestoreapi.com/products/categories')
       .then(response => {
         if (!response.ok) {
@@ -15,27 +14,30 @@ const ProductCategories = () => {
       })
       .then(data => {
         setCategories(data);
+        setError(null);
       })
       .catch(error => {
-        console.error('Error fetching categories: ', error);
+        setError(error.message);
       });
   }, []);
 
   return (
-    <section className="py-8">
-      <div className="container mx-auto text-center">
-        <h2 className="text-2xl font-bold text-gray-800 mb-4">Shop by Category</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-          {categories.map((category, index) => (
-            <div key={index} className="group relative bg-white p-4 shadow-md rounded-md overflow-hidden">
-              <img src={category} alt={category} className="w-full h-40 object-cover" />
-              <p className="text-lg font-semibold text-gray-800 mt-2 group-hover:text-orange-500 transition-colors duration-300">
-                {category}
-              </p>
+    <section className="py-44">
+        {error === null ? 
+          <div className="container mx-auto text-center">
+            <h2 className="text-2xl font-bold text-gray-800 mb-8">Shop by Category</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+              {categories.map((category, index) => (
+                <div key={index} className="group relative bg-white p-4 shadow-md rounded-md overflow-hidden">
+                  <p className="text-lg font-semibold text-gray-800 mt-2 group-hover:text-orange-500 transition-colors duration-300">
+                    {category.toUpperCase()}
+                  </p>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
+          </div>
+          : null
+      }
     </section>
   );
 };
